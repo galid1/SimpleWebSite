356cd8ece9a6f21aacff587e911574f0498f1404
jjy <galid1@naver.com>
2018-12-27 12:08:29 +0900

    DB변경  - SQLite -> Mysql

--- Web/Web/settings.py
+++ Web/Web/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'Web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE' : 'django.db.backends.mysql',
+		'NAME': 'testdb',
+		'USER': 'django',
+		'PASSWORD': 'It1',
+		'HOST': '192.168.0.247',
+		'PORT': '3306'
     }
 }
 
