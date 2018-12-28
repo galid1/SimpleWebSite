8d32937c75951bc694848ac31fe45e5e4a811ae7
jjy <galid1@naver.com>
2018-12-28 09:17:45 +0900

    1. model.py  - class WebUser:      user_pw 패스워드 암호화 저장을 위해 제한길이 상향

--- Web/authentication/models.py
+++ Web/authentication/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class WebUser(models.Model):
     user_id = models.CharField(max_length=10, unique=True)
-    user_pw = models.CharField(max_length=10)
+    user_pw = models.CharField(max_length=50)
 
 class Board(models.Model):
     board_title = models.CharField(max_length=30)
